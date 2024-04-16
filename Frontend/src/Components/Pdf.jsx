7fc6a88112d6c97acf995a12841804d4827b83cd
Nikhil <nikhilagrawal1876@gmail.com>
2024-04-16 21:48:41 +0530

    FirstProject

---
+++ Frontend/src/Components/Pdf.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ChatOpenAI } from "@langchain/openai";
+
+const chatModel = new ChatOpenAI({});
+
+const Pdf = () => {
+  return (
+    <div>Pdf</div>
+  )
+}
+
+export default Pdf
